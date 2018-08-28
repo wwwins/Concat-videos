f87312eef74b32f6768bd2a717844bf51a68021d
wwwins <jacky.huang@isobar.com>
2018-08-28 16:21:31 +0800

    Convert video to mp4 Setting fps=30

--- tools/segment.sh
+++ tools/segment.sh
@@ -3,20 +3,24 @@
 # Copyright 2018 isobar. All Rights Reserved.
 #
 # Usage:
-#       ./segment.sh base.h264 base-1
+#       ./segment.sh base.mp4 base-1
 #
 echo '>>>>>Start segmenting videos<<<<<'
 
 if [ -z "$1" ] 
   then
-    echo "Usage:\n      ./segemnt.sh base.h264 base-1"
+    echo "Usage:\n      ./segemnt.sh base.mp4 base-1"
     exit 1
 fi
 
-folder=~/intel-8th/data/
+folder=~/intel-8th/data/fps29.97-base/
 src=$1
-seg=$2-%03d.h264
+src30=fps30-$src
+seg=$2-%03d.mp4
 
 cd $folder
-ffmpeg -i $src -segment_time 2 -g 60 -sc_threshold 0 -force_key_frames "expr:gte(t,n_forced*60)" -f segment -strict -2 -preset veryfast $seg 
+ffmpeg -hide_banner -y -i $src -filter:v "setpts=0.999*PTS" -r 30 $src30
+ffmpeg -i $src30 -segment_time 2 -g 60 -sc_threshold 0 -force_key_frames "expr:gte(t,n_forced*60)" -f segment -strict -2 -preset veryfast $seg 
+mv $2-*.mp4 ../fps30/
 
+echo "\nrunning get-ts-video.sh\n"
