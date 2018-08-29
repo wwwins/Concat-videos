5fae0dee221103832ca892cf022f5c779079ed22
wwwins <jacky.huang@isobar.com>
2018-08-29 14:10:32 +0800

    Added dynamic variable names in bash

--- tools/generator.sh
+++ tools/generator.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright 2018 isobar. All Rights Reserved.
 #
 # Usage:
 #       ./generator.sh 01234 pic 
 #
-echo '>>>>>Starting generating frames'
+echo '>>>>>Start generating frames'
 
 if [ -z "$1" ]
   then
@@ -17,6 +17,7 @@ project=~/intel-8th/
 data=${project}data
 fps=1/2
 sno=$1
+int_sno=10#$1
 list=$sno/list
 audio=$data/audio.aac
 
@@ -26,24 +27,32 @@ text2=$data/text2.ts
 text3=$data/text3.ts
 end=$data/end.ts
 
-base0=$data/base-000.ts
-base1=$data/base-001.ts
-base2=$data/base-002.ts
-base3=$data/base-003.ts
-base4=$data/base-004.ts
-base5=$data/base-005.ts
-base6=$data/base-006.ts
+pid=$((int_sno%5))
+concat_params=concat_params$pid
 
+# 10 cuts
+for ((i=0; i<10; i++)); do
+declare base$i=$data/base-00$i.ts
+done
+
+# camera-1
 uts0=$sno/out-1-000.ts
 uts1=$sno/out-1-001.ts
-uts2=$sno/out-2-000.ts
-uts3=$sno/out-2-001.ts
+uts2=$sno/out-1-002.ts
+uts3=$sno/out-1-003.ts
+uts4=$sno/out-1-004.ts
+# camera-2
+uts5=$sno/out-2-000.ts
+uts6=$sno/out-2-001.ts
+uts7=$sno/out-2-002.ts
+uts8=$sno/out-2-003.ts
+uts9=$sno/out-2-004.ts
 
-concat_params1="concat:$intro|$base0|$base1|$uts0|$text1|$base2|$base3|$uts1|$uts2|$text2|$base4|$base5|$uts3|$base6|$end"
-concat_params2="concat:$intro|$uts0|$text1|$uts1|$text2|$uts2|$text3|$uts3|$end"
-concat_params3="concat:$intro|$uts0|$text1|$uts1|$text2|$uts2|$text3|$uts3|$end"
-concat_params4="concat:$intro|$uts0|$text1|$uts1|$text2|$uts2|$text3|$uts3|$end"
-concat_params5="concat:$intro|$uts0|$text1|$uts1|$text2|$uts2|$text3|$uts3|$end"
+concat_params0="concat:$intro|$base0|$base1|$uts0|$text1|$base2|$base3|$uts5|$uts1|$text2|$base4|$base5|$uts6|$base6|$end"
+concat_params1="concat:$intro|$base3|$base2|$uts1|$text1|$base1|$base4|$uts6|$uts2|$text2|$base6|$base5|$uts7|$base7|$end"
+concat_params2="concat:$intro|$base9|$base3|$uts2|$text1|$base7|$base5|$uts7|$uts3|$text2|$base6|$base4|$uts8|$base8|$end"
+concat_params3="concat:$intro|$base5|$base7|$uts3|$text1|$base9|$base1|$uts8|$uts4|$text2|$base3|$base2|$uts9|$base6|$end"
+concat_params4="concat:$intro|$base6|$base2|$uts4|$text1|$base9|$base3|$uts9|$uts0|$text2|$base7|$base1|$uts5|$base4|$end"
 
 src1=$sno-1.mp4
 src2=$sno-2.mp4
@@ -88,7 +97,7 @@ do
 done
 # merge video and audio
 echo ">>>>>MERGE VIDEO"
-ffmpeg -hide_banner -i $concat_params1 -c copy -y $anoutput
+ffmpeg -hide_banner -i ${!concat_params} -c copy -y $anoutput
 echo ">>>>>MERGE AUDIO"
 ffmpeg -hide_banner -i $anoutput -i $audio -c copy -y $output
 
